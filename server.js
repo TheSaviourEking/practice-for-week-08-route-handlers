01e98b8ae4bd5cebbb42c2dca8e8fd1c8d575e42
TheSaviourEking <saviour.eking@outlook.com>
2024-01-17 03:31:09 +0100

    [chore] implement the route handlers for the http server with node's native http server lib

--- server.js
+++ server.js
@@ -33,8 +33,63 @@ const server = http.createServer((req, res) => {
     }
     // Do not edit above this line
 
-    // define route handlers here
+      // define route handlers here
+      // GET /
+      if (req.method === 'GET' && req.url === '/') {
+	  res.statusCode = 200;
+	  res.setHeader('content-Type', 'text/plain');
+	  return res.end('Dog Club');
+      }
 
+      // GET /dogs
+      if (req.method === 'GET' && req.url === '/dogs') {
+	  res.statusCode = 200;
+	  res.setHeader('content-Type', 'text/plain');
+	  return res.end('Dog Index');
+      }
+
+      // /dogs/:dogid
+      if (req.method === 'GET' && req.url.startsWith('/dogs')){
+	  const reqArr = req.url.split('/');
+	  if (reqArr.length === '/shapes/:shapeName'.split('/').length) {
+	  res.statusCode = 200;
+	      res.setHeader('content-Type', 'text/plain');
+	      if (!Number(reqArr.slice(-1))) {
+		  // dog create form
+		  // GET /dogs/new
+		  return res.end('Dog create form page');
+	      }
+	      return res.end('dog details for ' + reqArr.slice(-1));
+	  }
+      }
+
+      // POST /dogs
+      if (req.method === 'POST' && req.url === '/dogs') {
+	  res.statusCode = 302;
+	  res.setHeader('Location', `/dogs/${getNewDogId()}`);
+	  return res.end();
+      }
+
+      // POST /dogs/:dogId
+      if (req.method === 'POST' && req.url.startsWith('/dogs')) {
+	  const reqArr = req.url.split('/');
+	  if (reqArr.length === '/dogs/:dogId'.split('/').length) {
+	      res.statusCode = 302;
+	      res.setHeader('Location', `/dogs/${reqArr.slice(-1)}`);
+	      return res.end();
+	  }
+      }
+
+      // GET /dogs/:dogId/edit
+      if (req.method === 'GET' && req.url.startsWith('/dogs')) {
+	  const reqArr = req.url.split('/');
+	  if (reqArr.slice(-1) == 'edit'){
+	      res.statusCode = 200;
+	      res.setHeader('content-Type', 'text/plain');
+	      return res.end('dog edit form page for ' + reqArr[2]);
+	  }
+      }
+     
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
     res.statusCode = 404;
